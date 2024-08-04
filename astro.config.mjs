c77c561cdcffd5b6ec4ead095499dc07ec12af2e
Daniel <contato.daniel.chiudini@gmail.com>
2024-08-04 08:52:03 -0300

    removed deploy from github pages and added deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://devdanielch.github.io',
   base: '/feriados-brasil',
   output: "hybrid",
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  adapter: netlify()
 });
